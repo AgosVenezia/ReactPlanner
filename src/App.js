2dd833cdedcdac05402d3e2ec78491ecf3a5a372
Agostina Celina Venezia Napolillo <agostina.cv@gmail.com>
2023-07-05 12:26:53 -0300

    CSS changes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import WidgetGalleryModal from './modals/WidgetGalleryModal';
 import Draggable from 'react-draggable';
 import TimerWidget from './widgets/TimerWidget';
 import CalendarWidget from './widgets/CalendarWidget';
+import ReminderListWidget from './widgets/ReminderListWidget';
  
 function App() {
  const [widgets, setWidgets] = useState([
   { id: new Date().getTime(), component: <ClockWidget />, area: 'main-widget', name: "Date and Time" },
   { id: new Date().getTime() + 2, component: <CalendarWidget />, area: 'right-widget', name: "Calendar" },
   { id: new Date().getTime() + 1, component: <TimerWidget />, area: 'left-widget', name:"Timer" },
+  { id: new Date().getTime() + 1, component: <ReminderListWidget />, area: 'left-widget', name:"Reminders" },
+
  ])
  const [showWidgetModal, setShowWidgetModal] = useState(false)
  const [selectedWidgetArea, setSelectedWidgetArea] = useState('')
